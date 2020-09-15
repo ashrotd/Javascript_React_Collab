347a732790ed4dfabb80391fdd0dc0b109aa3602
ashrotd <dhakalrabin04@gmail.com>
2020-09-15 21:55:13 +0545

    API added in javascript using fetch and catch

--- NewsPortal/main.js
+++ NewsPortal/main.js
@@ -1,1 +1,28 @@
-console.log('Setup Complete')
+var API_URL = 'http://newsapi.org/v2/everything?q=bitcoin&from=2020-08-15&sortBy=publishedAt&apiKey=API_KEY';
+
+function getNewsData() {
+        fetch(API_URL,{
+        method : 'GET',
+        }).
+        then(response=> response.json()).
+        then(data=>displayDataFromApi(data)).
+        catch(function (error) {
+            document.getElementById("loader").style.display= 'none';
+            document.getElementById('error').style.display = 'block';
+    });
+}
+function displayDataFromApi(data) {
+    var html = '';
+    data.articles.forEach(function (value) {
+       html += `<a href=\`+value.url+\` target="_blank"><div style="margin-top: 20px">
+            <div class="title">${value.title}</div>
+            <img src=\`+value.urlToImage+\` class="image">
+            <div class="news-fotter">
+                <div>\`+value.source.name+\` : <br/> \`+value.author+\`
+                </div>
+                <div>\`+value.publishedAt+\`</div>
+            </div>
+            <div class="contnet">\`+value.content+\`</div>
+        </div></a>\`;`
+    });
+}
